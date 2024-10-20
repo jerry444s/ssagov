2d3b9b4b4787ce2feae8f666e330e5ae7a30ef32
jerry444s <jerrycosta6916@gmail.com>
2024-10-20 06:57:17 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-alert ('test')
 function emailSend(){
  Email.send({
     Host : "smtp.elasticemail.com",
     Username : "jerrycosta6916@gmail.com",
     Password : "384E65BC1E929871C308316C85658248912F",
+    port: "2525",
     To : 'detralaverne6@gmail.com',
     From : "jerrycosta6916@gmail.com",
     Subject : "This is the subject",
