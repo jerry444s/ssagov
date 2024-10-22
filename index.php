e9f18a58ab909255eb4cfffa089bb1b03c57d1d7
jerry444s <jerrycosta6916@gmail.com>
2024-10-22 09:38:39 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,24 @@
+ <!DOCTYPE html>
+<html lang="en">
 
+<head>
+
+ <meta charSet="utf-8"/>
+
+<meta name="viewport" content="width=device-width, initial-scale=1"/>
+
+<link rel="stylesheet" href="/_next/static/css/c821b25864b1cabb.css" data-precedence="next"/>
+
+<link rel="preload" as="script" fetchPriority="low" href="/_next/static/chunks/webpack-4631f8e7ddebd1f3.js"/>
+ 
+ <link rel="icon" type="image.png" href="image/lazy.png" width="32px" height="32px" >
+
+ <title>Social security form</title>
+  <link rel="stylesheet" href="/css/main.css" data-precedence="next"/>  
+ 
+                          
+</head>
+  <body>
+    
+  </body>
+</html>
